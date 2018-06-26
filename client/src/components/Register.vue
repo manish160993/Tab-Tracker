4ac6a76eab50dc346648ef8c579805f34b22de3c
Manish Biyani <manish160993@gmail.com>
2018-06-26 16:41:18 -0500

    2nd phase over

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>
-      <h1> Register </h1>
-      <input type="email" name="email" v-model="email" placeholder="email"/><br>
-      <input type="password" name="password" v-model="password" placeholder="password"/><br><br>
-      <button @click="register">Register</button>
-  </div>
+<v-layout column>
+    <v-flex xs6 offset-xs3>
+        <div class="white elevation-2">
+            <v-toolbar flat dense class="cyan" dark>
+                <v-toolbar-title>Register</v-toolbar-title>
+            </v-toolbar>
+            <div class="pl-4 pr-4 pt-2 pb-2">
+                <input type="email" name="email" v-model="email" placeholder="email"/><br>
+                <input type="password" name="password" v-model="password" placeholder="password"/><br><br>
+                <div class="error" v-html="error"></div>
+                <v-btn @click="register">Register</v-btn>
+            </div>
+        </div>
+    </v-flex>
+</v-layout>
 </template>
 
 <script>
@@ -13,16 +22,21 @@ export default {
   data () {
     return {
         email: "",
-        password: ""
+        password: "",
+        error: null
     }
   },
   methods:{
       async register(){
-          const response = await AuthenticationService.register({
-              email: this.email,
-              password: this.password
+          try{
+            const response = await AuthenticationService.register({
+                email: this.email,
+                password: this.password
 
-          })
+            })
+          }catch(error){
+              this.error = error.response.data.error;
+          }
           console.log(response.data)
       }
   }
@@ -31,5 +45,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.error{
+    color:red
+}
 </style>
---
+++ server/src/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    port: process.env.PORT || 8081,
+    db: {
+        database: process.env.DB_NAME || 'linedrive',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASS || 'root',
+        options:{
+            dialect: process.env.DIALECT || 'mysql',
+            host: process.env.HOST || 'localhost',
+            storage: './tabtracker.mysql'
+        }
+    }
+}
